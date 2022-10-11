4c6aa3aaedd651146afd9d80c7f05187870401fe
Your Name <you@example.com>
2022-10-10 21:44:43 -0700

    yuh

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 # Make your Code
 # ******************************
 import random
-
+x=0
 numbers1 = []
 numbers2 = []
 result = []
 for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
+	numbers1.append(random.randint(0,10))
+	numbers2.append(random.randint(0,10))
+while x<10:
+	result.append(numbers1[x]+numbers2[x])
+	x=x+1
 print (numbers1)
 print (numbers2)
+print(result)
